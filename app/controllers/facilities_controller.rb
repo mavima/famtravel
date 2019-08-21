c76e8836d6414654f5315881678ecbdbc5fe76fd
Xinyi Wu <xxinyiwho@gmail.com>
2019-08-21 18:04:24 +0200

    FIX: search still has some problem, fix tomorrow

--- app/controllers/facilities_controller.rb
+++ app/controllers/facilities_controller.rb
@@ -2,7 +2,7 @@ class FacilitiesController < ApplicationController
   def index
     @facilities = Facility.all
     @facilities = @facilities.joins(:city).where("cities.name ILIKE ?", params[:search][:keyword]) if params[:search][:keyword].present?
-    @facilities = @facilities.joins(:category).where("categories.id = ?", params[:search][:category][1]) if params[:search][:category].present?
+    @facilities = @facilities.joins(:category).where("categories.id = ?", params[:search][:category]) if params[:search][:category].present?
   end
 
   def new
