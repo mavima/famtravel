675f91512d775ee827896754cac43b5cd26ed8a2
Xinyi Wu <xxinyiwho@gmail.com>
2019-08-22 13:51:38 +0200

    FEAT: review works

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,11 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   resources :categories, only: [:index, :new, :create] do
-    resources :facilities, only: [:new, :create, :show]
+    resources :facilities, only: [:new, :create]
   end
-  resources :facilities, only: [:index, :show, :new, :create]
+  resources :facilities, only: [:index, :show, :new, :create] do
+    resources :reviews, only: [:new, :create, :edit, :update]
+  end
+
   resources :city, only: [:new, :create]
 end
