bc311e4bae188dcddfb372d6c8f77679ca9a9646
Maria Manninen <maria.vihla@gmail.com>
2019-08-23 12:57:14 +0200

    Had to change the seed for cities as country has been renamed country_city

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,9 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
   def home
-    @playground = Category.where(name: "Playground").first
-    @activity = Category.where(name: "Activity").first
-    @restaurant = Category.where(name: "Restaurant").first
-    @restroom = Category.where(name: "Restroom").first
+    @playground = Category.find_by(name: "Playground")
+    @activity = Category.find_by(name: "Activity")
+    @restaurant = Category.find_by(name: "Restaurant")
+    @restroom = Category.find_by(name: "Restroom")
   end
-
 end
