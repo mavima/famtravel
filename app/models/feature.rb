e1b0a565c22b462e59ce2bf37f892c613d5762d1
Josephine Geoghegan <josephineanngeoghegan@gmail.com>
2019-08-27 09:47:14 +0200

    seeds completed add facilityfeature

--- app/models/feature.rb
+++ app/models/feature.rb
@@ -1,5 +1,6 @@
 class Feature < ApplicationRecord
   has_many :feature_categories, dependent: :destroy
+  has_many :feature_facilities, dependent: :destroy
   has_many :categories, through: :feature_category
   has_many :facilities, through: :feature_facilities
 
